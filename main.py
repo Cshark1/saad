42d3872815cddcf4fd90b28a0512c51d91aa45ee
BuildTools <unconfigured@null.spigotmc.org>
2022-02-21 17:34:19 +0200

    added input prompt for api key

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from termcolor import colored
 import xml.etree.ElementTree as ET
 import os
 
@@ -10,12 +11,34 @@ steam_id_64 = "76561198267351741"
 game_id = "271590"
 nsfw = "false"
 
+
+
+while True:
+    api_key = input(colored("Enter API key (Available at https://www.steamgriddb.com/profile/preferences): ", "white"))
+    r = requests.get("https://www.steamgriddb.com/api/v2/grids/steam/720",
+                     headers={"Authorization": "Bearer {}".format(api_key)})
+    if r.status_code == 401:
+        print(colored("Invalid API Key!", "red"))
+    else:
+        print(colored("The API key is valid!", "green"))
+        break
+
+
+
+
+
+
+"""
 owned_games_xml = ET.fromstring(requests.get(owned_games_url.format(steam_id_64)).text)
 for x in owned_games_xml[2]:
+     print("Geting image for game {}".format(x[1].text))
+     r = requests.get(path.format(game_id), headers={"Authorization": "Bearer {}".format(api_key)},
+                      params={"types": "animated", "nsfw": nsfw})
+     if r.status_code == 404:
+          print("Game '{}' not found".format(x[1].text))
+     if r.json()["data"][0]["url"] == []:
+          print("No artwork found for game {}".format(x[1].text))
+     print(r.json()["data"][0]["url"])
      print(x[0].text)
+"""
 
-
-#r = requests.get(path.format(game_id), headers={"Authorization": "Bearer {}".format(api_key)},
-#                 params={"types": "animated", "nsfw": nsfw})
-
-#print(r.json())
